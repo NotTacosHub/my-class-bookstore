051785a787ac0001a84d78499d6124b4f0fbe1df
NotTacosHub <smithclayton80@gmail.com>
2021-11-03 11:07:51 -0500

    added schema and classes

--- app.py
+++ app.py
@@ -6,7 +6,20 @@ import os
 
 app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" +os.path.join(basedir, "app.site")
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" +os.path.join(basedir, "app.sqlite")
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 CORS(app)
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False, unique=True)
+    author = db.Column(db.String, nullable=False)
+    review = db.Column(db.String(144), nullable=False)
+    genre = db.Column(db.String, nullable=True)
+
+    def __init__(self, title, author, review, genre):
+        self.title = title
+        self.author = author
+        self.review = review
+        self.genre = genre
